a6d8f9679ae4fb32b1e0a0493e3c3046f39c1d38
Ma7moud_Wael <79937116+7od@users.noreply.github.com>
2026-08-12 22:05:54 +0300

    adding contentbstructure

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,8 +1,45 @@
+import { useRef } from "react";
 import styles from "../../cssModules/content.module.css";
 const Content = () => {
+	const inputRef = useRef(null);
+	const taskContainerRef = useRef(null);
+	const subButton = (e) => {
+		e.preventDefault();
+		if (inputRef.current.value) {
+			const cliked = document.createElement("div");
+			cliked.className = styles.cliked;
+			cliked.innerHTML = inputRef.current.value;
+
+			const del = document.createElement("button");
+			del.className = styles.del;
+			del.innerText = "del";
+			del.onclick = function (e) {
+				e.preventDefault();
+				del.parentElement.remove();
+			};
+
+			cliked.appendChild(del);
+			taskContainerRef.current.appendChild(cliked);
+			// localStorage.setItem("a", taskContainerRef.current.innerHTML);
+			inputRef.current.value = "";
+		}
+	};
 	return (
 		<div className={styles.content_container}>
-			<h1>hello from contnet</h1>
+			<form>
+				<input
+					type='text'
+					ref={inputRef}
+				/>
+				<button
+					type='submit'
+					onClick={subButton}>
+					Add Task
+				</button>
+			</form>
+			<div
+				className={styles.tasks_container}
+				ref={taskContainerRef}></div>
 		</div>
 	);
 };
